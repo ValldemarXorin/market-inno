de6debd2542f6340a479cd2b5a980d56420641aa
ValldemarXorin <vova3089927@gmail.com>
2026-07-21 12:54:37 +0300

    feature(user-srvice-db-scheme): created BaseEntity with auditing, User entity and PaymentCard entity

---
+++ user-service/src/main/java/inno/user_service/entity/PaymentCard.java
@@ -0,0 +1,38 @@
+package inno.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "payment_cards")
+public class PaymentCard extends BaseEntity {
+
+    @Id
+    @UuidGenerator(style = UuidGenerator.Style.VERSION_7)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 19)
+    private String number;
+
+    @Column(nullable = false, length = 100)
+    private String holder;
+
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+}
---
+++ user-service/src/main/java/inno/user_service/entity/User.java
@@ -0,0 +1,41 @@
+package inno.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity{
+
+    @Id
+    @UuidGenerator(style = UuidGenerator.Style.VERSION_7)
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String surname;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PaymentCard> paymentCards = new ArrayList<>();
+}
