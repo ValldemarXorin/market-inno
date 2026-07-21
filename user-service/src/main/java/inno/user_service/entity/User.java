84430b0e855c280baf3c3dd25914fda952ea329c
ValldemarXorin <vova3089927@gmail.com>
2026-07-21 13:23:44 +0300

    feature(user-service-db-scheme): added validation to entity field on hibernate level

--- user-service/src/main/java/inno/user_service/entity/User.java
+++ user-service/src/main/java/inno/user_service/entity/User.java
@@ -1,6 +1,7 @@
 package inno.user_service.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -21,18 +23,28 @@ public class User extends BaseEntity{
     @UuidGenerator(style = UuidGenerator.Style.VERSION_7)
     private UUID id;
 
+    @NotBlank
+    @Size(max = 100)
     @Column(nullable = false, length = 100)
     private String username;
 
+    @NotBlank
+    @Size(max = 100)
     @Column(nullable = false, length = 100)
     private String surname;
 
+    @NotNull
+    @Past
     @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
+    @NotBlank
+    @Email
+    @Size(max = 255)
     @Column(nullable = false, unique = true, length = 255)
     private String email;
 
+    @NotNull
     @Column(nullable = false)
     private Boolean active = true;
 
