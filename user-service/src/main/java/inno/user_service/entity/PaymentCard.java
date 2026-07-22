fcc5189afde87e867ceab5d7f22dd80a5e7365b7
ValldemarXorin <vova3089927@gmail.com>
2026-07-22 13:46:35 +0300

    fix(user-service-remove-redundant-entity-validation): removed redudant entity validation code because it covered by dto validations

--- user-service/src/main/java/inno/user_service/entity/PaymentCard.java
+++ user-service/src/main/java/inno/user_service/entity/PaymentCard.java
@@ -21,27 +21,19 @@ public class PaymentCard extends BaseEntity {
     @UuidGenerator(style = UuidGenerator.Style.VERSION_7)
     private UUID id;
 
-    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @NotBlank
-    @Pattern(regexp = "\\d{13,19}", message = "Card number must contain 13 to 19 digits")
     @Column(nullable = false, length = 19)
     private String number;
 
-    @NotBlank
-    @Size(max = 100)
     @Column(nullable = false, length = 100)
     private String holder;
 
-    @NotNull
-    @FutureOrPresent
     @Column(name = "expiration_date", nullable = false)
     private LocalDate expirationDate;
 
-    @NotNull
     @Column(nullable = false)
     private Boolean active = true;
 }
