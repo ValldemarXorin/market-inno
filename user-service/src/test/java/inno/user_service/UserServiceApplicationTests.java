6ec9a951c69fec9ea843dc1872dd37e00a9a8106
ValldemarXorin <vova3089927@gmail.com>
2026-07-23 19:49:42 +0300

    test: fix default contextLoads test using testcontainers

--- user-service/src/test/java/inno/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/inno/user_service/UserServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class UserServiceApplicationTests extends UserControllerIntegrationTest {
 
 	@Test
 	void contextLoads() {
