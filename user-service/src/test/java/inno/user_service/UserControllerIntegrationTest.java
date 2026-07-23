772d1af97f863fafe4e35b2e27fa674cd3d8c036
ValldemarXorin <vova3089927@gmail.com>
2026-07-24 01:50:50 +0300

    added /api/v1 prefix in integration tests

--- user-service/src/test/java/inno/user_service/UserControllerIntegrationTest.java
+++ user-service/src/test/java/inno/user_service/UserControllerIntegrationTest.java
@@ -65,7 +65,7 @@ public class UserControllerIntegrationTest {
                 "masha@yandex.ru"
         );
 
-        String responseContent = mockMvc.perform(post("/users")
+        String responseContent = mockMvc.perform(post("/api/v1/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(requestDto)))
                 .andExpect(status().isCreated())
@@ -76,18 +76,17 @@ public class UserControllerIntegrationTest {
         UserResponse createdUser = objectMapper.readValue(responseContent, UserResponse.class);
         String idFromDb = createdUser.id().toString();
 
-        mockMvc.perform(get("/users/" + idFromDb))
+        mockMvc.perform(get("/api/v1/users/" + idFromDb))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(idFromDb))
                 .andExpect(jsonPath("$.username").value("Masha"));
 
-        mockMvc.perform(get("/users/" + idFromDb))
-                .andDo(print())
+        mockMvc.perform(get("/api/v1/users/" + idFromDb))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(idFromDb));
 
-        mockMvc.perform(get("/users/00000000-0000-0000-0000-000000000000"))
+        mockMvc.perform(get("/api/v1/users/00000000-0000-0000-0000-000000000000"))
                 .andExpect(status().isNotFound());
     }
 }
